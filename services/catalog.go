628caec0cd810ac8016fdea222c92fb55380f00c
songvut365 <songvut.nakrong@gmail.com>
2022-07-25 15:41:48 +0700

    Add json tag

--- services/catalog.go
+++ services/catalog.go
@@ -1,9 +1,9 @@
 package services
 
 type Product struct {
-	ID       int
-	Name     string
-	Quantity int
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
 }
 
 type CatalogService interface {
